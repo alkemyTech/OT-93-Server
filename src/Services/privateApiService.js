b472ac614d30a4dc76673c4ec5cc68e1d06aba94
joaquinnieva <joacoo.nieva@gmail.com>
2021-10-26 20:26:32 -0300

    method DELETE created

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
-const config = {
+const API = process.env.REACT_APP_API_URL;
+
+//function for add authorization in header
+const getToken = (token) => {
+  const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+      Group: "Grupo 93",
+      Accept: "*/*",
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+  };
+  return config;
+};
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+// DELETE
+const Delete = (URI, id, token) => {
+  //set config
+  const config = getToken(token);
+  // using axios for petition
+  axios
+    .delete(`${API}/${URI}/${id}`, config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-export default Get
+export { Delete };
