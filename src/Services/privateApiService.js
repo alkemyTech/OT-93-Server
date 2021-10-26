6d623e1dee39d255cb47cc72f21de72f25a654a1
joaquinnieva <joacoo.nieva@gmail.com>
2021-10-26 20:16:08 -0300

    method POST created and dotenv installed

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,38 +1,40 @@
 import axios from "axios";
+
+const API = process.env.REACT_APP_API_URL;
+
+//function for add authorization in header
+const getToken = (token) => {
+  const config = {
+    headers: {
+      Group: "Grupo 93",
+      Accept: "*/*",
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+  };
+  return config;
+};
+
+// POST
 const Post = (URI, body, token) => {
   //set config
-  const config = {
-    headers: {
-      // "Grupo":  93,
-      "User-Agent": "Grupo 93",
-      Accept: "*/*",
-      "Content-Type": "application/json",
-      Authorization: token,
-    },
-  };
+  const config = getToken(token);
   // using axios for petition
   axios
-    .post(URI, body, config)
+    .post(`${API}/${URI}`, body, config)
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 };
 
-const Delete = (URI, token) => {
+// DELETE
+const Delete = (URI, id, token) => {
   //set config
-  const config = {
-    headers: {
-      // "Grupo":  93,
-      "User-Agent": "Grupo 93",
-      Accept: "*/*",
-      "Content-Type": "application/json",
-      Authorization: token,
-    },
-  };
+  const config = getToken(token);
   // using axios for petition
   axios
-    .delete(URI, config)
+    .delete(`${API}/${URI}/${id}`, config)
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
 };
 
-export {Post,Delete} ;
+export { Post, Delete,API };
