c15b76b6ffce867794c809abfeaceeaf2f081ed3
Joaquín Nieva <78621100+joaquinnieva@users.noreply.github.com>
2021-10-27 16:12:08 -0300

    fix hardcoding of api url

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,9 +1,6 @@
 import axios from "axios"; //import axios
 import getToken from "../utils/getToken"; //import gettoken
 
-// getting API-URL
-const API = process.env.REACT_APP_API_URL;
-
 // POST
 export const Post = async (URI, body) => {
   //getting token
@@ -20,7 +17,7 @@ export const Post = async (URI, body) => {
   };
 
   try {
-    let response = await axios.post(`${API}/${URI}`, body, header);
+    let response = await axios.post(`${process.env.REACT_APP_API_URL}/${URI}`, body, header);
     return JSON.stringify(response);
   } catch (error) {
     console.log(error);
