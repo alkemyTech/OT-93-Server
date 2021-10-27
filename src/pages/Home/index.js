b0194992bb7fba425822f92f41cc6730a17789b3
Andres pro <andres.prommel@redtecnologica.org>
2021-10-27 01:10:10 -0300

    add-example-core

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import fromState from '../../core/selectors';
+import Component from './Component';
+
+export default connect(
+    state => ({
+       //aca van los selectos
+    }),
+    dispatch => bindActionCreators({
+        //aca van los actions
+    }, dispatch)
+)(Component);
