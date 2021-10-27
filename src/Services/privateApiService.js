a84f19636819484c81b65535c59924dd05173430
joelgomezok <joelgomez995@gmail.com>
2021-10-27 17:03:04 -0300

    Creando metodo reutilizable que realice una peticion PARCH a los endopoints privados de la API

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,33 @@
 import axios from 'axios';
 
 const config = {
-    headers: {
-        Group: '93'
+    headers: { 
+        Group: 93
     }
 }
 
-const get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+const Get = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users', config) 
+    .then(res => console.log(res)) 
+    .catch(err => console.log(err)) 
 }
 
-export default get
+export default Get
+
+export const Patch = async (URI, id, body) => {
+    const token = getToken();
+    const header = {
+      headers: {
+        Group: "Grupo 93",
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+    };
+    try {
+      let response = await axios.Patch(`${API}/${URI}${id}`, body, header);
+      return response;
+    } catch (error) {
+      return error;
+    }
+  };
