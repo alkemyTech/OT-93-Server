77a1bade97916c7f3809f0a4b99f7188539babf4
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2021-10-27 20:01:12 -0300

    Website header styles

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap'
 import { authUser } from '../utils/index';
 import { publicHeaderDataArray, privateHeaderDataArray } from '../utils/index'
+import './header.css'
+import ongLogo from '../images/LOGO-SOMOS MAS.png'
 
 const Header = () => {
   const publicLinks = publicHeaderDataArray
@@ -16,16 +19,29 @@ const Header = () => {
   }
   console.log(window.location.pathname === '/')
   return (
-    <nav>
-      {userAuthenticated ?
-        privateLinks.map(link => {
-          return <Link to={link.path} className={checkPath(link.path)}>{link.label}</Link>
-        })
-        : publicLinks.map(link => {
-          return <Link to={link.path} className={checkPath(link.path)}>{link.label} </Link>
-        })
-      }
-    </nav>
+    <nav className='container-fluid p-0'>
+      <Container>
+        <Row>
+          <Col lg={2} >
+            <img className='header-logo' src={ongLogo} alt="Logo ONG" />
+          </Col>
+          <Col lg={7} className='d-flex flex-row align-items-center'>
+            {userAuthenticated ?
+              privateLinks.map(link => {
+                return <Link to={link.path} className={`m-2  ${checkPath(link.path)}`}>{link.label}</Link>
+              })
+              : publicLinks.map(link => {
+                return <Link to={link.path} className={`m-2  ${checkPath(link.path)}`}>{link.label} </Link>
+              })
+            }
+          </Col>
+          <Col lg={3} className='d-flex flex-row align-items-center justify-content-end'>
+            <button className='btn header-button-login m-1 ' outline>Log in</button>
+            <button className='btn header-button-register m-1 ' outline >Registrarse</button>
+          </Col>
+        </Row>
+      </Container>
+    </nav >
   );
 }
 
