ac4695d5c51091c910426169407261fcf0d10446
Joaquin Abregu <abregujoaquin@gmail.com>
2021-10-27 15:29:54 -0300

    OT93-71 - put method

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 01, //Aqui va el ID del equipo!!
+  },
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get("https://jsonplaceholder.typicode.com/users", config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-export default Get
+export default Get;
+
+export const Put = async (URI, id, body) => {
+  const token = getToken();
+  const header = {
+    headers: {
+      Group: "Grupo 93",
+      Accept: "*/*",
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
+  };
+
+  try {
+    let response = await axios.put(`${API}/${URI}${id}`, body, header);
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
