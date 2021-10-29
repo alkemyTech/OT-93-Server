c76769e46701514f37911949af09be7222ab0cae
joelgomezok <joelgomez995@gmail.com>
2021-10-29 13:40:18 -0300

    fix patch

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,48 +1,18 @@
 import axios from 'axios';
 
-const config = {
-  headers: {
-    Group: 93,
-  },
-};
-
-const Get = () => {
-  axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
-};
-export default Get;
-
-export const Patch = async (URI, id, body) => {
-  const token = getToken();
-  const header = {
-    headers: {
-      Group: 'Grupo 93',
-      Accept: '*/*',
-      'Content-Type': 'application/json',
-      Authorization: token,
-    },
-  };
+export const patch = async (URI, id, body) => {
+  const config = getTokenHeader();
   try {
-    const response = await axios.Patch(`${API}/${URI}${id}`, body, header);
+    const response = await axios.Patch(`${API}/${URI}${id}`, body, config);
     return response;
   } catch (error) {
     return error;
   }
 };
 export const Delete = async (URI, id) => {
-  const token = getToken();
-  const header = {
-    headers: {
-      Group: 'Grupo 93',
-      Accept: '*/*',
-      'Content-Type': 'application/json',
-      Authorization: token,
-    },
-  };
-
+  const config = getTokenHeader();
   try {
-    const response = await axios.post(`${API}/${URI}/${id}`, header);
+    const response = await axios.post(`${API}/${URI}/${id}`, config);
     return JSON.stringify(response);
   } catch (error) {
     return (error);
