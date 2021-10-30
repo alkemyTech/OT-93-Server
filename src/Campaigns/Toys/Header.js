15bfba0eda35b68366d6462d47fbdcaa9a646393
Ignacio Emiliano Juarez <emij_13@hotmail.com>
2021-10-29 21:20:45 -0300

    Toys campaign header

--- src/Campaigns/Toys/Header.js
+++ src/Campaigns/Toys/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import toysCampaignLogo from '../../utils/assets/toysCampaignLogo.png';
+import '../../css/CampaignHeader.css';
+import CampaignHeader from '../../Components/Campaigns/Header/index';
 
 const Header = () => {
+  const sloganText = 'Lorem ipsum dolor sit amet.';
   return (
-    <div>
-      
-    </div>
+    <CampaignHeader campaignLogo={toysCampaignLogo} slogan={sloganText} />
   );
-}
- 
+};
+
 export default Header;
