913c8c23c678cb24c754ccde3eb98d1cb03369d3
joaquinnieva <joacoo.nieva@gmail.com>
2021-10-31 19:59:25 -0300

    method DELETE created

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,25 +1,11 @@
-import axios from "axios"; //import axios
-import getToken from "../utils/getToken"; //import gettoken
+import axios from 'axios';
 
-// DELETE
 export const Delete = async (URI, id) => {
-  //getting token
-  const token = getToken();
-  //set header
-  const header = {
-    headers: {
-      Group: "Grupo 93",
-      Accept: "*/*",
-      "Content-Type": "application/json",
-      Authorization: token,
-    },
-  };
-
+  const config = getTokenHeader();
   try {
-    let response = await axios.post(`${API}/${URI}/${id}`, header);
-    return JSON.stringify(response);
+    const response = await axios.delete(`${API}/${URI}/${id}`, config);
+    return response;
   } catch (error) {
-    console.log(error);
+    return error;
   }
 };
-
