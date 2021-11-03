e8982be9061ee31cd0526c61ec9a1752242bb0c9
Martiniano Sanchi <77789053+deztidev@users.noreply.github.com>
2021-11-03 12:00:38 -0300

    Fixed bootstrap standard

--- src/Components/Title/index.js
+++ src/Components/Title/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 const Title = ({ text, onlyTitle, image }) => (
-  <Container>
-    <h1>{text}</h1>
-    {!onlyTitle && (
-      <img
-        style={{ position: 'relative', bottom: '56px', zIndex: -1 }}
-        src={image}
-        alt="Title background"
-      />
-    )}
-  </Container>
+  <Row>
+    <Col>
+      <h1>{text}</h1>
+      {!onlyTitle && (
+        <img
+          style={{ position: 'relative', bottom: '56px', zIndex: -1 }}
+          src={image}
+          alt="Title background"
+        />
+      )}
+    </Col>
+  </Row>
 );
 
 Title.propTypes = {
