bfcba0b5ff2b209ab1bacb6ee72b12b30f009416
joaquinnieva <joacoo.nieva@gmail.com>
2021-11-04 18:40:30 -0300

    form fixing in router

--- src/app/router.js
+++ src/app/router.js
@@ -3,8 +3,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { getRoutes } from '../utils';
 import Home from '../pages/Home';
-// import NewForm from '../pages/News/Form';
-// import NewList from '../pages/News/List';
 import Register from '../pages/Register';
 // import ActivitiesForm from '../Components/Activities/ActivitiesForm';
 // import CategoriesForm from '../Components/Categories/CategoriesForm';
@@ -23,11 +21,8 @@ const backOfficeRoutes = getRoutes('mainRoutes');
 function Router() {
   return (
     <Switch>
-      <Route exact path={mainRoutes.home} component={Register} />
+      <Route exact path={mainRoutes.home} component={Home} />
       <Route path={mainRoutes.register} component={Register} />
-      {/* <Route exact path={mainRoutes.newsForm} component={NewForm} />
-      <Route exact path={`${mainRoutes.newsForm}/:id`} component={NewForm} />
-      <Route exact path={mainRoutes.news} component={NewList} /> */}
       {/* <Route path="/create-activity" component={ActivitiesForm} />
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
