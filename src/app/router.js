d6b303c8442ffdaf57b5f0d6abf5750ac712dc47
Andres pro <andres.prommel@redtecnologica.org>
2021-11-08 11:31:12 -0300

    conflicts

--- src/app/router.js
+++ src/app/router.js
@@ -18,7 +18,6 @@ function Router() {
   return (
     <Switch>
       <Route exact path={publicRoutes.home} component={Home} />
-      <Route exact path={publicRoutes.news} component={News} />
       <Route exact path={publicRoutes.newsForm} component={NewForm} />
       <Route exact path={`${publicRoutes.newsForm}/:id`} component={NewForm} />
       <Route exact path={mainRoutes.home} component={Home} />
