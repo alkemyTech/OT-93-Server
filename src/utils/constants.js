ae14e2b72e19a20e19ae5c8ef4eeac5815f1fd21
Joaquin Abregu <abregujoaquin@gmail.com>
2021-11-11 16:32:32 -0300

    pull

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,6 +23,4 @@ export const Confirm = 'Confirmar';
 export const Cancel = 'Cancelar';
 export const Empty = 'Empty';
 export const TESTIMONIALS = 'Testimonios';
-export const ACTIVITIES = 'Actividades';
 export const SLIDES = 'Slides';
-
