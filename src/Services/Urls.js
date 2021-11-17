ff17f142ca4af41df4641a148c8616ee094d5de2
Jorge Gorostegui Di Peto <jorge.gdp@gmail.com>
2021-11-17 17:46:20 -0300

    Small PR changes

--- src/Services/Urls.js
+++ src/Services/Urls.js
@@ -1,10 +1,10 @@
+export const USERS = 'users';
 export const HOME = '/';
 export const NEWS = 'news';
 export const ORGANIZATION = 'organization';
 export const SLIDES = 'slides';
 export const ACTIVITIES = 'activities';
 export const CATEGORIES = 'categories';
-export const USERS = 'users';
 export const TESTIMONIALS = 'testimonials';
 export const PROJECTS = 'projects';
 export const MEMBERS = 'members';
