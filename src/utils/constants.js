8683477213dd9f12f2a48621ff44724070a7674f
joaquinnieva <joacoo.nieva@gmail.com>
2021-11-24 15:45:31 -0300

    agregue una condicional para que suscribirse sea estando logeado

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -56,3 +56,4 @@ export const INVALID_EMAIL = 'Dirección de e-mail inválida';
 export const SHORT_PHONE_NUMBER = 'El teléfono debe contener 8 números como mínimo';
 export const NO_FETCH = 'Hubo un problema con el servicio';
 export const NO_ACTION = 'No se pudo realizar la acción, intenta más tarde';
+export const NO_LOGGED = 'Antes debes estar registrado';
