ff7d49fc7b56998405c76f86463bf9c758dea893
deztidev <martinianosanchi@gmail.com>
2021-11-27 10:51:28 -0300

    Added Slider in Toys Campaign

--- src/Campaigns/Toys/Slider.js
+++ src/Campaigns/Toys/Slider.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import firstPhoto from '../../images/Foto-10.jpg';
+import secondPhoto from '../../images/Foto-11.jpg';
+import thirdPhoto from '../../images/Manos-10.jpg';
+import Carrousel from '../../Components/Slider';
 
-const Slider = () => (
-    <div />
-);
+const Slider = () => {
+  const slides = [
+    {
+      name: 'Foto 10',
+      description: 'Jugando',
+      image: firstPhoto,
+    },
+    {
+      name: 'Foto 11',
+      description: 'Corriendo',
+      image: secondPhoto,
+    },
+    {
+      name: 'Manos 10',
+      description: 'Dibujando',
+      image: thirdPhoto,
+    },
+  ];
+  return (
+    <Carrousel items={slides} />
+  );
+};
 
 export default Slider;
