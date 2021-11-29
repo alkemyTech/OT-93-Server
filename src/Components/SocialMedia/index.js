2d0ad4cc58a872ad75bc4d65c66bdd04543c65af
Luis Pirela <34918215+contrastereo@users.noreply.github.com>
2021-11-29 09:45:36 -0300

    Added socialmedia component to the us page

--- src/Components/SocialMedia/index.js
+++ src/Components/SocialMedia/index.js
@@ -1,20 +1,31 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { Col, Row, Container } from 'reactstrap';
-import { LinkedinCompanyProfile, TwitterTweet } from 'react-social-plugins';
+import { Link } from 'react-router-dom';
+import { AiFillLinkedin, AiFillTwitterSquare } from 'react-icons/ai';
+import { TwitterTweet } from 'react-social-plugins';
 import { ONG_NAME } from '../../utils/constants';
 
 export default function SocialMedia() {
   return (
+
     <Container>
       <Row>
-        <Col>
-          <LinkedinCompanyProfile
-            lang="en_US"
-            companyId={3144678}
-            format="inline"
-            text={ONG_NAME}
-          />
+        <Col className="d-flex flex-column align-items-center justify-content-center">
+          <Link
+            to="/"
+            style={{ textDecoration: 'none', fontWeight: '600', fontSize: '36px' }}
+            className="d-flex align-items-center justify-content-center"
+          >
+          <AiFillLinkedin style={{ marginRight: '4px' }} /> {ONG_NAME}
+          </Link>
+          <Link
+            to="/"
+            style={{ textDecoration: 'none', fontWeight: '600', fontSize: '36px' }}
+            className="d-flex align-items-center justify-content-center"
+          >
+          <AiFillTwitterSquare style={{ marginRight: '4px' }} /> {ONG_NAME}
+          </Link>
         </Col>
         <Col>
           <TwitterTweet
